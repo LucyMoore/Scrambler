81449dd3504eba1b1f41ca4be4df4343d33f4df9
Mel & Suresh Swamy <pair+melissa-c.sureshnswamy@devbootcamp.com>
2016-04-01 10:55:05 +1300

    psuedocode for scramble

---
+++ scramble.js
@@ -0,0 +1,28 @@
+var
+
+module.exports = scramble function  () {
+
+var wordArr = []
+
+// receive the original unscrambled word from POSt req from client
+//do the scramble logic
+//convert word by split() into array
+//change the individual alphabets index in the array
+//constraint every alpha cannot take their original index
+//join the scrambled alpha's into word string
+
+// POST the scrambled word back to client
+
+//when the client types in solution receive it as a POST req
+//check case => convert it to UPPERCASE
+//verify it with ORIginal word
+//If correct send Correct message
+//if not send wrong message
+
+//if they hit help =>>> do what?
+
+
+
+
+
+}

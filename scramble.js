8f53355d92cf0c7af7cff74da2440a720aa5cf0e
Mel & Suresh Swamy <pair+melissa-c.sureshnswamy@devbootcamp.com>
2016-04-01 14:55:17 +1300

    scramble returns undefined sometimes

--- scramble.js
+++ scramble.js
@@ -3,50 +3,52 @@
 // var request = require('request');
 
 // module.exports =
-function scramble (req, res){
-
+function scramble (req){
+//receive the original unscrambled word from POST req from client
 // var origWord = JSON.stringify(req.body.xxx)
+
+//convert word with split() into an array
 var origArr = req.split('')
 var maxLen = origArr.length
 
-var scramArr = req.split('')
-
-// for(var i = 0; i < maxLen; i++){
-//   origArr.indexOf(origArr[i])
-// }
-
-for(var i = 0; i < maxLen; i++){
-  for(var j = 0; j < parseInt(Math.random() * maxLen); j++){
-    var x = origArr[i]
-    origArr[i] = origArr[j]
-    origArr[j] = x
-  // console.log("x:", x, "j:", j, "origArr:", origArr )
-  }
+//assign a new index to each element in the array
+    for(var i = 0; i <= maxLen; i++){
+      for(var j = 0; j <= Math.floor(Math.random() * maxLen); j++){
+        var x = origArr[i]
+        origArr[i] = origArr[j]
+        origArr[j] = x
+        }
+        var scrambled = origArr.join('')
+      }
+        console.log('x', x, 'j', j, 'i', i, 'arr', origArr)
+   var result = check(scrambled, req)
+   console.log('result:', result, '!!!')
+   return result
 }
 
-//if scrambled = req, then run again
-var joined = origArr.join('')
-  console.log("scrambled:", joined, req)
-  return origArr.join()
+function check(scrambled, req){
+  if(scrambled == req || scrambled == undefined){
+    console.log('words equal', scrambled,'=', req)
+    scramble(scrambled)
+  } else {
+  console.log('checked:', scrambled)
+  return scrambled
+}
+}
+//check('cat', 'cat')
+scramble('cat')
 
-
-
-// receive the original unscrambled word from POSt req from client
-//do the scramble logic
-//convert word by split() into array
-//change the individual alphabets index in the array
-//constraint every alpha cannot take their original index
-//join the scrambled alpha's into word string
+//join the newly scrambled array into a word string
+// var scrambled = origArr.join('')
+//if scrambled === req, then run loop again
 
 // POST the scrambled word back to client
 
 //when the client types in solution receive it as a POST req
-//check case => convert it to UPPERCASE
-//verify it with ORIginal word
-//If correct send Correct message
-//if not send wrong message
+//ignore case when checking if solution === original word
+//if they match then true
+//if not then false
 
 //if they hit help =>>> do what?
 
-}
-scramble('treasure')
+
